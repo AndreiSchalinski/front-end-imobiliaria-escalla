15fa0db396411f9ffad61c3b22c35f4e8947d8a3
AndreiSchalinski <andreischalinski@hotmail.com>
2025-02-28 20:59:29 -0300

    Implementação de legenda para imóveis

---
+++ src/views/site-principal/SitePrincipalImoveisCarousel.jsx
@@ -0,0 +1,48 @@
+export default function SitePrincipalImoveisCarousel() {
+  return (
+    <div className="container-site-principal-informacoes-imovel">
+      <div className="container-site-principal-informacoes-imovel-cliente">
+        <ul className="container-site-principal-informacoes-imovel-cliente-icones-medidas">
+          <li>Ícone</li>
+          <li>Ícone</li>
+          <li>Ícone</li>
+          <li>Ícone</li>
+          <li>Ícone</li>
+        </ul>
+
+        <div>
+          <h3>O que você vai encontrar nesse imóvel:</h3>
+          <p>
+            Cozinha | integrada | Área de serviço | Suíte máster | Banheira de
+            hidromassagem | Vista mar | Sacada integrada | Espaço gourmet |
+            Lavabo Living integrado | Quadra mar
+          </p>
+        </div>
+
+        <div>
+          <h3>O que você vai encontrar nesse empreendimento:</h3>
+          <p>
+            Piscina | Playground | Salão de festas | Salão de jogos |
+            Brinquedoteca | Espaço | Fitness | Espaço | Gourmet | Cinema | Sala
+            de Massagem | Spa Sala de descanso | Sauna úmida
+          </p>
+        </div>
+
+        <div>
+          <h3>Sobre este imóvel</h3>
+          <p>
+            CONDIÇÃO ESPECIAL - Apartamento Duplex no Edifício Magnifique Tower
+            em Balneário Camboriú com 227m² privativos, 03 suítes + 01 suíte
+            master com banheira de hidromassagem, lavabo, living integrado com
+            sala de estar e sala de jantar, amplo mezanino, cozinha, sacada
+            integrada com espaço gourmet, área de serviço e 03 vagas de garagem.
+          </p>
+          <a href="">Exibir Mais +</a>
+        </div>
+      </div>
+      <div className="container-site-principal-informacoes-imovel-cliente">
+        <h1>Botões de contato</h1>
+      </div>
+    </div>
+  );
+}
