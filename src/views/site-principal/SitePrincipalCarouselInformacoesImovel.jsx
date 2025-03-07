4f1c8c1e7440de730a8b57f5b8bd512b9cd7e7de
AndreiSchalinski <andreischalinski@hotmail.com>
2025-03-07 13:03:38 -0300

    Implementação de botões para navegação do carousel large

--- src/views/site-principal/SitePrincipalCarouselInformacoesImovel.jsx
+++ src/views/site-principal/SitePrincipalCarouselInformacoesImovel.jsx
@@ -1,9 +1,53 @@
 import CarouselLargeImovel from "@/components/site-principal/CarouselLargeImovel";
 
+import Imagem1 from "../../assets/testee.jpeg";
+import Imagem2 from "../../assets/testee.jpeg";
+import Imagem3 from "../../assets/testee.jpeg";
+import Imagem4 from "../../assets/testee.jpeg";
+import Imagem5 from "../../assets/testee.jpeg";
+import Imagem6 from "../../assets/testee.jpeg";
+import Imagem7 from "../../assets/testee.jpeg";
+import Imagem8 from "../../assets/testee.jpeg";
+import Imagem9 from "../../assets/testee.jpeg";
+import Imagem10 from "../../assets/testee.jpeg";
+import Imagem11 from "../../assets/testee.jpeg";
+import Imagem12 from "../../assets/testee.jpeg";
+import Imagem13 from "../../assets/testee.jpeg";
+import Imagem14 from "../../assets/testee.jpeg";
+import Imagem15 from "../../assets/testee.jpeg";
+import Imagem16 from "../../assets/testee.jpeg";
+import Imagem17 from "../../assets/testee.jpeg";
+import Imagem18 from "../../assets/testee.jpeg";
+import Imagem19 from "../../assets/testee.jpeg";
+import Imagem20 from "../../assets/testee.jpeg";
+
+const listImgs = [
+  Imagem1,
+  Imagem2,
+  Imagem3,
+  Imagem4,
+  Imagem5,
+  Imagem6,
+  Imagem7,
+  Imagem8,
+  Imagem9,
+  Imagem10,
+  Imagem11,
+  Imagem12,
+  Imagem13,
+  Imagem14,
+  Imagem15,
+  Imagem16,
+  Imagem17,
+  Imagem18,
+  Imagem19,
+  Imagem20,
+];
+
 export default function SitePrincipalCarouselInformacoesImovel() {
   return (
     <div className="testekk">
-      <CarouselLargeImovel />
+      <CarouselLargeImovel listImgs={listImgs}/>
     </div>
   );
 }
