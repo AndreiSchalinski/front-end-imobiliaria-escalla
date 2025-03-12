4d09c96734ee03cf0310d296965187f308cb7023
AndreiSchalinski <andreischalinski@hotmail.com>
2025-03-12 10:51:58 -0300

    Implementação de styles para texts e icons

--- src/views/site-principal/SitePrincipalSobreEdificio.jsx
+++ src/views/site-principal/SitePrincipalSobreEdificio.jsx
@@ -1,4 +1,17 @@
 import CarouselGridImoveis from "@/components/site-principal/CarouselGridImoveis";
+import { getIcon } from "@/assets/icons";
+
+const listCaracteristicas = [
+  <p>{getIcon().iconBed}&nbsp; 3 ou 4 Dormitórios &nbsp; </p>,
+  <p>{getIcon().iconShower}&nbsp; 3 ou 4 Suítes &nbsp; </p>,
+  <p>{getIcon().iconRuler}&nbsp; de 160.21m² a 276m² &nbsp; </p>,
+  <p>{getIcon().iconCar}&nbsp; 2 ou 3 vagas de garagem &nbsp; </p>,
+  <p>{getIcon("#5783EB").iconHome}&nbsp; 3 Unidades disponíveis &nbsp; </p>,
+  <p>
+    {getIcon().iconLocation}&nbsp; Carlos Alberto Niehus 82, Praia Brava - SC
+    &nbsp;{" "}
+  </p>,
+];
 
 export default function SitePrincipalSobreEdificio() {
   return (
@@ -6,35 +19,20 @@ export default function SitePrincipalSobreEdificio() {
       <div className="container-site-principal-imoveis-informacoes-edificios">
         <div className="container-site-principal-imoveis-informacoes-edificios-detalhes">
           <h1>Sobre o Edifício Brava Aikon</h1>
-          <p>O que você vai encontrar:</p>
-          <p>
+          <p className="container-site-principal-imoveis-informacoes-edificios-detalhes-text-short">
+            O que você vai encontrar:
+          </p>
+          <p className="container-site-principal-imoveis-informacoes-edificios-detalhes-text-short-2">
             Playground, Salão de festas, Salão de jogos, Espaço Fitness, Espaço
             Gourmet, Solarium
           </p>
+          <div className="container-site-principal-imoveis-informacoes-edificios-detalhes-list-caracteristicas">
+            {listCaracteristicas.map((el) => {
+              return el;
+            })}
+          </div>
           <div>
-            <ul>
-              <li>
-                <p>3 ou 4 Dormitórios</p>
-              </li>
-              <li>
-                <p>3 ou 4 Suítes</p>
-              </li>
-              <li>
-                <p>de 160.21m² a 276m²</p>
-              </li>
-              <li>
-                <p>2 ou 3 vagas de garagem</p>
-              </li>
-              <li>
-                <p>3 Unidades disponíveis</p>
-              </li>
-              <li>
-                <p>Carlos Alberto Niehus 82, Praia Brava - SC</p>
-              </li>
-              <li>
-                <a href="">Conheça o edifício</a>
-              </li>
-            </ul>
+            <span>Conheça o edifício</span>
           </div>
         </div>
         <div className="container-site-principal-imoveis-informacoes-edificios-detalhes">
