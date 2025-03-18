ef5381d236af56fb318e362eb76dad86a7a8d8d9
AndreiSchalinski <andreischalinski@hotmail.com>
2025-03-18 15:14:55 -0300

    Implementação de sinal sum corretores

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -527,5 +527,16 @@ export const getIcon = (color) => {
         />
       </svg>
     ),
+    iconSum: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 -960 960 960"
+        width="24px"
+        fill={color == undefined ? "#333A44" : color}
+      >
+        <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+      </svg>
+    ),
   };
 };
