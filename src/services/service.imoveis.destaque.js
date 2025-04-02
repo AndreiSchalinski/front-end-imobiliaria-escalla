d4b71d044bb3339aba288604ccc10aae2d94f09b
AndreiSchalinski <andreischalinski@hotmail.com>
2025-04-02 15:36:01 -0300

    Implementação url http

--- src/services/service.imoveis.destaque.js
+++ src/services/service.imoveis.destaque.js
@@ -2,7 +2,7 @@ import customAxios from "./serive.customAxios";
 
 export const getImoveisDestaques = async () => {
   return customAxios()
-    .get("http://escallaimoveis.com:8080/imoveis/altaprocura")
+    .get("https://escallaimoveis.com:8080/imoveis/altaprocura")
     .then((imovel) => {
       console.log(imovel);
     });
