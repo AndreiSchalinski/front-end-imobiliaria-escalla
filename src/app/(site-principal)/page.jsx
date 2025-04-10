b4b8d1c5b168c16845ca093573fa4af04fdafc92
AndreiSchalinski <andreischalinski@hotmail.com>
2025-04-10 17:19:54 -0300

    Immplementacao remove endpoint

--- src/app/(site-principal)/page.jsx
+++ src/app/(site-principal)/page.jsx
@@ -38,7 +38,7 @@ export default function Home() {
   
   useEffect(() => {
     const fetchData = async () => {
-      await getImoveisDestaques();
+      //await getImoveisDestaques();
     };
   
     fetchData();
