389bdf06f950508d336f1151724ba96a0f569c4f
AndreiSchalinski <andreischalinski@hotmail.com>
2025-04-14 14:59:38 -0300

    Implementação de correção para urls-cors-endpoint

--- src/services/service.imoveis.destaque.js
+++ src/services/service.imoveis.destaque.js
@@ -2,6 +2,6 @@ import customAxios from "./serive.customAxios";
 
 export const getImoveisDestaques = async () => {
   return customAxios()
-    .get("http://localhost:8080/imoveis/altaprocura")
+    .get("http://escallaimoveis.com:8080/imoveis/altaprocura")
     .then((imovel) => {console.log(imovel)});
 };
