dd874d6d759bae4606e07e19fb6b6e9c0972dbfe
AndreiSchalinski <andreischalinski@hotmail.com>
2025-04-15 10:53:51 -0300

    Implementação de uri endpoint

--- src/services/service.imoveis.destaque.js
+++ src/services/service.imoveis.destaque.js
@@ -2,6 +2,6 @@ import customAxios from "./serive.customAxios";
 
 export const getImoveisDestaques = async () => {
   return customAxios()
-    .get("http://escallaimoveis.com:8080/imoveis/altaprocura")
+    .get("https://escallaimoveis.com/api/imoveis/altaprocura")
     .then((imovel) => {console.log(imovel)})
 };
