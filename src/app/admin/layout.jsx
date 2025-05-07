66a47b85fc79e55b4e8910517d8ff61783da2b82
AndreiSchalinski <andreischalinski@hotmail.com>
2025-05-07 08:44:42 -0300

    Implementação de form login

---
+++ src/app/admin/layout.jsx
@@ -0,0 +1,30 @@
+import "../../style/global.css";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import "swiper/css";
+import "swiper/css/grid";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+export const metadata = {
+  title: "Page admin",
+  description: "Essa página é admin",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@40,400,0,0"
+        />
+      </head>
+
+      <body>
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
+    </html>
+  );
+}
