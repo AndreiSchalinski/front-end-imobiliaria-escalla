b4a7e54c0943a24a35191658f123d8ffa9263d94
AndreiSchalinski <andreischalinski@hotmail.com>
2025-06-08 14:22:24 -0300

    Implementação de correção folha text artigo

--- src/app/artigo/[id]/page.jsx
+++ src/app/artigo/[id]/page.jsx
@@ -1,15 +1,17 @@
+import SitePrincipalFooter from "@/components/site-principal/Footer";
 import HeaderSite from "@/components/site-principal/Header";
 import { getArtigo } from "@/services/service.artigos";
 import ArtigoFolhaPage from "@/views/artigo/ArtigoFolhaPage";
 
 export default async function Home({ params }) {
-  const { id } = params;
-  const { data: artigo } = await getArtigo(id); 
+  const id = params.id;
+  const { data: artigo } = await getArtigo(id);
 
   return (
     <>
       <HeaderSite />
       <ArtigoFolhaPage artigo={artigo} />
+      <SitePrincipalFooter />
     </>
   );
 }
